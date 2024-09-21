97fad1f223922f300e1b7b0749188b76411b5824
ramya14741 <ramya14741@gmail.com>
2024-09-21 13:41:42 -0300

    few  more concepts

---
+++ reversearray.js
@@ -0,0 +1,15 @@
+function reverseArray(arr){
+    let start = 0;
+    let end = arr.length-1;
+    while(start<end){
+        let temp = arr[start];
+        arr[start]= arr[end];
+        arr[end]=temp;
+        start++;
+        end--;
+    }
+    return arr;
+}
+
+let arr =[1,2,3,4,5];
+console.log(reverseArray(arr));
